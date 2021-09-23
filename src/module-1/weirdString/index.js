398e474b0dd6bcd7ea75b5d8d67c4c5f9effd9d2
Dmytro Pasika <dmytro.pasika@gmail.com>
2021-09-23 18:09:20 +0300

    add solution for modul-1

--- src/module-1/weirdString/index.js
+++ src/module-1/weirdString/index.js
@@ -1,1 +1,2 @@
-export const weirdString = (str = "") => {};
+export const weirdString = (str = "") => str.split(' ').map(item => item.split('').map((char, index, arr) => index !== arr.length - 1 ? char.toUpperCase() : char).join('')).join(' ');
+    
