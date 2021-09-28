31743d5a9ffe4a46a23b9dc76c10c6d6243d8f27
Dmytro Pasika <dmytro.pasika@gmail.com>
2021-09-28 19:04:43 +0300

    add card-list module-3

--- src/module-3/cards-list-v1/index.js
+++ src/module-3/cards-list-v1/index.js
@@ -1,9 +1,39 @@
-import Card from '../../module-2/card/index.js';
-
 export default class CardsList {
-  constructor (data = []) {
-    this.data = data;
+  element;
 
-    // ... your logic
+  constructor({ data = [], Component = {} }) {
+    this.data = data;
+    this.Component = Component;
+
+    this.render();
+  }
+
+  getTemplate(){
+  return `<div class="cards__list">
+    ${this.data.map(d => new this.Component(d).element.innerHTML).join('')}
+    </div>`
+}
+
+  render() {
+    const wrapper = document.createElement('div');
+
+    wrapper.innerHTML = this.getTemplate();
+    this.element = wrapper;
+  }
+
+  update(arr) {
+    this.data = arr;
+    this.render();
+  }
+
+  remove(id = 0) {
+    this.data = this.data.filter(d => d.id !== id);
+    this.render();
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
   }
 }
+
