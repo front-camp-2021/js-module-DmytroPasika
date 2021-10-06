90df3955369c3a25eda2018d4dff25bca775d33f
Dmytro Pasika <dmytro.pasika@gmail.com>
2021-10-06 13:06:18 +0300

    add double slider & pagination

--- src/module-5/double-slider/index.js
+++ src/module-5/double-slider/index.js
@@ -1,4 +1,5 @@
 export default class DoubleSlider {
+  element;
   constructor({
     min = 100,
     max = 200,
@@ -10,6 +11,75 @@ export default class DoubleSlider {
     precision = 0,
     filterName = ''
   } = {}) {
-    // ... your logic
+    
+    this.max = max;
+    this.min = min;
+    this.selectedMin = selected['from'];
+    this.selectedMax = selected['to'];
+    this.step = precision;
+    this.percent1 = 0;
+    this.percent2 = 100;
+    this.formatValue1 = function (price) { return formatValue(price)}
+    this.formatValue2 = function (price) { return formatValue(price)}
+    this.title = filterName;
+    this.render();
+  }
+
+  get template() {
+    console.log(this.formatValue1)
+    console.log(this.formatValue2)
+    console.log('percent 1 ====', this.percent1)
+    console.log('percent 2 ====', this.percent2)
+    return `<div class="filter-container__slider-container">
+        <div class="filter-container__title">
+          ${this.title}
+        </div>
+    <div id="range1" class='filter-container__range'>
+        ${this.formatValue1(this.selectedMin)}
+    </div>
+    <div class="filter-container__slider" style="background:">
+        <div class="filter-container__slider-track" style="background:linear-gradient(to right, rgb(218, 218, 229) ${this.percent1}% , rgb(111, 100, 248) ${this.percent1}% , rgb(111, 100, 248) ${this.percent2}%, rgb(218, 218, 229) ${this.percent2}%)"></div>
+        <input type="range" min="${this.min}" max="${this.max}" value="${this.selectedMin}" step="${this.step}" id="slider-1" class='filter-container__range-input-1'>
+        <input type="range" min="${this.min}" max="${this.max}" value="${this.selectedMax}" step="${this.step}" id="slider-2" class='filter-container__range-input-2'>
+    </div>
+    <div id="range2" class='filter-container__range'>
+    ${this.formatValue2(this.selectedMax)}
+    </div>
+    </div>`
+  }
+
+  
+
+  render() {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.template;
+    this.element = wrapper.firstElementChild;
+  }
+
+  slideOne(min = this.selectedMin){
+    this.selectedMin = parseInt(min);
+    this.fillColor();
+}
+
+  slideTwo(max = this.selectedMax){
+    this.selectedMax = parseInt(max);
+    this.fillColor();
+  }
+
+  fillColor(){
+    this.percent1 = ((this.selectedMin - this.min)  / (this.max - this.min)) * 100;
+    this.percent2 = ((this.selectedMax - this.min) / (this.max - this.min)) * 100;
+    this.render();
+  }
+
+  remove() {
+    if(this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
   }
 }
--- src/module-5/pagination/index.js
+++ src/module-5/pagination/index.js
@@ -1,31 +1,94 @@
 export default class Pagination {
   element;
   start = 0;
-  pageIndex = 0;
+  currentPage = 0;
 
   constructor({
     totalPages = 10,
-    page = 1,
+    currentPage = 1,
   } = {}) {
     this.totalPages = totalPages;
-    this.pageIndex = page - 1;
-
+    this.pageIndex = currentPage;
+    
+    this.render();
     // ... your logic
+  }
+
+  get template(){
+    
+    return `
+    <div class="pagination">
+      <div>
+        <a href="#" id='prevPage' class="pagination__reference-direction reference">
+          <img src="../img/PathNav2.svg" id="prevPageImg" alt="navigation path image">
+        </a>
+      </div>
+      <div class="pagination__pages reference">
+        ${this.pages().slice(this.start, this.start + 10).join('')}
+      </div>
+      <a href="#" id='nextPage' class="pagination__reference-direction reference">
+        <img src="../img/PathNav1.svg" id="nextPageImg" alt="navigation path image">
+      </a>
+    </div>
+    `;
+  }
+
+  pages() {
+    const pagesArr = [];
+    for (let index = 0; index < this.totalPages; index++) {
+      pagesArr.push(`
+      <div class="pagination__container ${this.pageIndex == index + 1? 'pagination__active' : '1'}">
+      <a href="#${index + 1}" id="pagination__id-${index + 1}" class="pagination__reference reference ${this.pageIndex == index + 1? 'pagination__active' : ''}">${index + 1}</a>
+      </div>
+      `)
+    }
+    return pagesArr;
+  }
+
+  render() {
+    const wrapper = document.createElement('div');
+    console.log('page->', this.pageIndex)
+    wrapper.innerHTML = this.template;
+    this.element = wrapper.firstElementChild;
+  }
+
+  takeCurPage(ref = []){
+    this.pageIndex = +ref[1];
+    this.render();
   }
 
   goToPrevPage () {
-    // ... your logic
+    if (this.pageIndex !== 0 && this.start > 0) {
+      this.start -= 1;
+    } 
+    if (this.pageIndex > 1) {
+      this.pageIndex = this.pageIndex - 1; 
+      this.render();
+    }
   }
 
   goToNextPage () {
-    // ... your logic
+    if (this.currentPage !== this.totalPages) {
+      
+      if (this.start < 10) {
+        this.start += 1;
+      } 
+    }
+    if (this.pageIndex < this.totalPages) {
+      this.pageIndex = this.pageIndex + 1;
+      this.render();
+    }
+  
   }
 
-  remove () {
-    // ... your logic
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
   }
 
-  destroy () {
-    // ... your logic
+  destroy() {
+    this.remove();
+    this.element = null;
   }
 }
