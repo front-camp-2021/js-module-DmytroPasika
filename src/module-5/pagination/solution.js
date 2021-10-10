ab192c2253669c172bd33ba5a986c465e5e5ed38
Dmytro Pasika <dmytro.pasika@gmail.com>
2021-10-10 22:32:32 +0300

    fix pagination

--- src/module-5/pagination/solution.js
+++ src/module-5/pagination/solution.js
@@ -9,13 +9,13 @@ export default class Pagination {
   } = {}) {
     this.totalPages = totalPages;
     this.pageIndex = currentPage;
-    
+
     this.render();
     // ... your logic
   }
 
-  get template(){
-    
+  get template() {
+
     return `
     <div class="pagination">
       <div>
@@ -33,12 +33,13 @@ export default class Pagination {
     `;
   }
 
+
   pages() {
     const pagesArr = [];
     for (let index = 0; index < this.totalPages; index++) {
       pagesArr.push(`
-      <div class="pagination__container ${this.pageIndex == index + 1? 'pagination__active' : '1'}">
-      <a href="#${index + 1}" id="pagination__id-${index + 1}" class="pagination__reference reference ${this.pageIndex == index + 1? 'pagination__active' : ''}">${index + 1}</a>
+      <div class="pagination__container ${this.pageIndex == index + 1 ? 'pagination__active' : '1'}">
+      <a href="#${index + 1}" id="pagination__id-${index + 1}" class="pagination__reference reference ${this.pageIndex == index + 1 ? 'pagination__active' : ''}">${index + 1}</a>
       </div>
       `)
     }
@@ -52,33 +53,33 @@ export default class Pagination {
     this.element = wrapper.firstElementChild;
   }
 
-  takeCurPage(ref = []){
+  takeCurPage(ref = []) {
     this.pageIndex = +ref[1];
     this.render();
   }
 
-  goToPrevPage () {
-    if (this.pageIndex !== 0 && this.start > 0) {
+  goToPrevPage() {
+    if (this.pageIndex !== 0 && this.start > 0 && this.pageIndex < (this.totalPages - 4)) {
       this.start -= 1;
-    } 
+    }
     if (this.pageIndex > 1) {
-      this.pageIndex = this.pageIndex - 1; 
+      this.pageIndex = this.pageIndex - 1;
       this.render();
     }
   }
 
-  goToNextPage () {
+  goToNextPage() {
     if (this.currentPage !== this.totalPages) {
       
-      if (this.start < 10) {
+      if (this.start < (this.totalPages - 10) && this.pageIndex > 4) {
         this.start += 1;
-      } 
+      }
     }
     if (this.pageIndex < this.totalPages) {
       this.pageIndex = this.pageIndex + 1;
       this.render();
     }
-  
+
   }
 
   remove() {
